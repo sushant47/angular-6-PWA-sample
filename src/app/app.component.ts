0810cf1b4fcd75c4e5f0972743600b04e495dcfd
Sushant <Administrator@EEEPC0387.extentia05.local>
2019-01-16 17:08:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  update: Boolean = false;
+  joke: any;
+
+  constructor(updates: SwUpdate, private dataService: DataService) {
+    updates.available.subscribe(event => {
+      // this.update = true;
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
+  ngOnInit() {
+    this.dataService.gimmeJokes().subscribe(res => {
+      this.joke = res;
+    });
+  }
 }
